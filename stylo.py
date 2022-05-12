ff8b55bd692442d1371d3eb8414ccc7ad4d53940
LemmensJens <jenslemmens97@gmail.com>
2022-05-12 11:32:13 +0200

    add PCA option ; adapt Grieve (2007) features to our needs

--- stylo.py
+++ stylo.py
@@ -1,29 +1,34 @@
-import pandas as pd
-import util
-import argparse
-import os
-import stanza
+import os, argparse
+
 from statistics import mean, stdev
 from collections import Counter
+from tqdm import tqdm
 
-#_______________________________________________________________
+import pandas as pd
+import numpy as np
+import stanza, util
+from sklearn.decomposition import PCA
+
+import plotly.express as px
+#______________________________________________________________________________________________
 
 def main(args):
 
 #LOAD DATA_____________________________________________________________________________________
-    if args.input[-4:] == '.txt': # 1 .txt file
+    input_format = args.input_format
+
+    if input_format == 'txt': # 1 .txt file
         with open(args.input) as f:
             lines = f.readlines()
             lines = [l.strip() for l in lines]
         texts = [' '.join(lines)]
         infiles = [args.input]
     
-    elif args.input[-4:] in {'.csv', '.xlsx'}: # 1 csv/excel file
+    elif input_format in {'xlsx', 'csv'}: # 1 csv/excel file
         if args.input[-4:] == '.csv':
             df = pd.read_csv(args.input)
         else:
             df = pd.read_excel(args.input)
-        df = df.head(2)
         texts = list(df[args.text_column])
         infiles = list(df.index)
     
@@ -40,12 +45,13 @@ def main(args):
                 text = ' '.join(lines)
                 texts.append(text)
     
-    if not args.overwrite_output_dir:
+    if args.overwrite_output_dir != 't':
         assert os.path.exists(output_dir) == False
 
     df_out = pd.DataFrame()
+    feature_df = pd.DataFrame()
 
-#PREPROCESSING_________________________________________________________________________________________
+#PREPROCESSING_________________________________________________________________________________
     try:
         nlp = stanza.Pipeline(lang='nl', processors='tokenize,pos', verbose=0)
     except: #TO DO: find prettier way of checking whether Dutch Stanza has been downloaded already
@@ -53,7 +59,7 @@ def main(args):
 
     nlp = stanza.Pipeline(lang='nl', processors='tokenize,pos', verbose=0)
     
-    for infile, text in zip(infiles, texts):
+    for infile, text in tqdm(zip(infiles, texts)):
         doc = nlp(text)
         parsed_sentences = [[(w.text, w.upos) for w in s.words] for s in doc.sentences]
         tokenized_sentences = [[t for t, pos in s if pos not in {'PUNCT', 'SYM', 'X'}] for s in parsed_sentences]
@@ -61,71 +67,59 @@ def main(args):
         types = set([t.lower() for t in tokens])
         syllables = [[util.get_n_syllables(t) for t, pos in s if pos not in {'PUNCT', 'SYM', 'X'}] for s in parsed_sentences]
 
-#STATISTICS_________________________________________________________________________________________
-        if args.statistics:
-            n_char = len(text)
-            n_syllables = sum([syl for sent in syllables for syl in sent])
-            n_polysyllabic = len([i for sent in syllables for i in sent if i > 1])
-            n_tokens = len(tokens)
-            n_longer_than_6_char = len([t for t in tokens if len(t) < 6])
-            n_types = len(types)
-            n_sentences = len(doc.sentences)
+#LENGTH STATISTICS_____________________________________________________________________________
+        n_char = len(text)
+        n_syllables = sum([syl for sent in syllables for syl in sent])
+        n_polysyllabic = len([i for sent in syllables for i in sent if i > 1])
+        n_tokens = len(tokens)
+        n_longer_than_6_char = len([t for t in tokens if len(t) < 6])
+        n_types = len(types)
+        n_sentences = len(doc.sentences)
 
-            avg_char_per_word = round(mean([len(t) for t in tokens]), 2)
-            std_char_per_word = round(stdev([len(t) for t in tokens]), 2)
+        avg_char_per_word = round(mean([len(t) for t in tokens]), 5)
+        std_char_per_word = round(stdev([len(t) for t in tokens]), 5)
 
-            avg_syl_per_word = round(mean([s for sent in syllables for s in sent]), 2)
-            std_syl_per_word = round(stdev([s for sent in syllables for s in sent]), 2)
+        word_length_distribution = util.get_word_length_distribution(tokens)
 
-            avg_words_per_sent = round(mean([len(s) for s in tokenized_sentences]), 2)
-            std_words_per_sent = round(stdev([len(s) for s in tokenized_sentences]), 2)
+        avg_syl_per_word = round(mean([s for sent in syllables for s in sent]), 5)
+        std_syl_per_word = round(stdev([s for sent in syllables for s in sent]), 5)
 
-            stats = {
-            'n_characters': n_char,
-            'n_syllables': n_syllables,
-            'n_tokens': n_tokens,
-            'n_polysyllabic_tokens': n_polysyllabic,
-            'n_long_tokens': n_longer_than_6_char, #(> 6 char)
-            'n_types': n_types,
-            'n_sentences': n_sentences,
-            'avg_characters_per_word': avg_char_per_word,
-            'std_characters_per_word': std_char_per_word,
-            'avg_syllables_per_word': avg_syl_per_word,
-            'std_syllables_per_word': std_syl_per_word,
-            'avg_words_per_sentence': avg_words_per_sent,
-            'std_words_per_sentence': std_words_per_sent
-            }
-        
-#DISTRIBUTIONS_________________________________________________________________________________________
-        if args.distributions:
-            word_length_distribution = util.get_word_length_distribution(tokens)
-            grapheme_distribution = util.get_grapheme_distribution(tokens)
-            word_internal_grapheme_profile = util.get_word_internal_grapheme_profile(tokens)
-            token_distribution = util.get_token_distribution(tokens)
-            grapheme_positional_freq = util.get_grapheme_positional_freq(tokens)
-            punct_dist = util.get_punct_dist(text)
-            bigram_profile = util.get_bigram_profile(tokens)
-            positional_word_profile = util.get_positional_word_profile(doc)
-            
-            dist = {
-                'word_length_distribution': word_length_distribution,
-                'unigram_distribution': token_distribution,
-                'bigram_distribition': bigram_profile,
-                'punctuation_distribution': punct_dist,
-                'positional_word_profile': positional_word_profile,
-                'grapheme_distribution': grapheme_distribution,
-                'word_internal_grapheme_profile': word_internal_grapheme_profile,
-                'grapheme_positional_distribution': grapheme_positional_freq
-            }
+        avg_words_per_sent = round(mean([len(s) for s in tokenized_sentences]), 5)
+        if n_sentences > 1:
+            std_words_per_sent = round(stdev([len(s) for s in tokenized_sentences]), 5)
+        else:
+            std_words_per_sent = 0
+        ratio_long_words = round(n_longer_than_6_char/n_tokens, 5)
 
-#LEXICAL RICHNESS_________________________________________________________________________________________
-        if args.lexical_richness:
+        stats = {
+        'n_characters': n_char,
+        'n_syllables': n_syllables,
+        'n_tokens': n_tokens,
+        'n_polysyllabic_tokens': n_polysyllabic,
+        'n_long_tokens': n_longer_than_6_char, #(> 6 char)
+        'n_types': n_types,
+        'n_sentences': n_sentences,
+        'avg_characters_per_word': avg_char_per_word,
+        'std_characters_per_word': std_char_per_word,
+        'avg_syllables_per_word': avg_syl_per_word,
+        'std_syllables_per_word': std_syl_per_word,
+        'ratio_long_words': ratio_long_words,
+        'avg_words_per_sentence': avg_words_per_sent,
+        'std_words_per_sentence': std_words_per_sent,
+        'word_length_distribution': word_length_distribution
+        }
+
+#LEXICAL RICHNESS______________________________________________________________________________
+        if args.lexical_richness == 'y':
             ttr = util.ttr(n_types, n_tokens)
             rttr = util.rttr(n_types, n_tokens)
             cttr = util.cttr(n_types, n_tokens)
             Herdan = util.Herdan(n_types, n_tokens)
             Summer = util.Summer(n_types, n_tokens)
-            Dugast = util.Dugast(n_types, n_tokens)
+            if n_types == n_tokens:
+                Dugast = 0
+            else:
+                Dugast = util.Dugast(n_types, n_tokens)
             Maas = util.Maas(n_types, n_tokens)
 
             lr = {
@@ -138,8 +132,8 @@ def main(args):
             'Maas': Maas
             }
 
-#READABILITY_________________________________________________________________________________________
-        if args.readability:
+#READABILITY___________________________________________________________________________________
+        if args.readability == 'y':
             ARI = util.ARI(n_char, n_tokens, n_sentences)
             CL = util.ColemanLiau(tokens, tokenized_sentences)
             Flesch = util.Flesch(avg_words_per_sent, avg_syl_per_word)
@@ -160,48 +154,97 @@ def main(args):
             'SMOG': SMOG
             }
 
-#POS-TAG DISTRIBUTION_________________________________________________________________________________________
-        if args.pos_distribution:
-            pos_tags = [pos for s in parsed_sentences for t, pos in s]
-            n_total = len(pos_tags)
-            pos_distribution = dict(Counter(pos_tags))
-            pos_distribution = {k:v/n_total for k,v in pos_distribution.items()}
+#DISTRIBUTIONS_________________________________________________________________________________
+        if args.distributions == 'y':
+            punct_dist = util.get_punct_dist(text)
+            function_word_distribution = util.get_function_word_distribution(doc)
+            pos_distribution = util.get_pos_distribution(parsed_sentences)
+            pos_bigram_distribution = util.get_pos_distribution(parsed_sentences, ngram_range=2)
+            
+            dist = {
+                'punctuation_distribution': punct_dist,
+                'function_word_distribution': function_word_distribution,
+                'pos_distribution': pos_distribution,
+                'pos_bigram_distribution': pos_bigram_distribution
+            }
 
-#WRITE RESULTS TO OUTPUT______________________________________________________________________________________
+#WRITE RESULTS TO OUTPUT_______________________________________________________________________
         row = dict()
 
-        row['filename'] = infile
-        for k, v in stats.items():
-            row[k] = v
-        for k, v in readability.items():
-            row[k] = v
-        for k, v in lr.items():
-            row[k] = v
-        for k, v in dist.items():
-            row[k] = str(v)
-        row['pos_distribution'] = str(pos_distribution)
+        row['file_id'] = infile
+        if args.statistics == 'y':
+            for k, v in stats.items():
+                row[k] = v
+        if args.lexical_richness == 'y':
+            for k, v in lr.items():
+                row[k] = v
+        if args.readability == 'y':
+            for k, v in readability.items():
+                row[k] = v
+        if args.distributions == 'y':
+            for k, v in dist.items():
+                row[k] = str(v)
 
         df_out = df_out.append(row, ignore_index=True)
+
+        if args.pca=='y': # group features slightly differently for pca
+            feature_row = dict()
+            feature_row['file_id'] = infile
+            if args.statistics == 'y':
+                for k, v in stats.items():
+                    if k == 'word_length_distribution':
+                        for length, n in v.items():
+                            feature_row[str(length)] = n
+                    else:
+                        feature_row[k] = v
+            if args.lexical_richness == 'y':
+                for k, v in lr.items():
+                    feature_row[k] = v
+            if args.readability == 'y':
+                for k, v in readability.items():
+                    feature_row[k] = v
+            if args.distributions == 'y':
+                for _, d in dist.items():
+                    for k, v in d.items():
+                        feature_row[str(k)] = str(v)
+            feature_df = feature_df.append(feature_row, ignore_index=True)
+
+    if args.pca == 'y' and len(df_out)>1:
+        feature_df.fillna(0, inplace=True)
+        columns_to_cast_to_integers = [c for c in feature_df.columns if c[:2]=='n_']
+        if type(infiles[0]) != str:
+            columns_to_cast_to_integers.append('file_id')
+        feature_df = feature_df.astype({k: 'int' for k in columns_to_cast_to_integers})
+        feature_df.to_csv('feature_df.csv', index=False)
     
     columns_to_cast_to_integers = [c for c in df_out.columns if c[:2]=='n_']
     if type(infiles[0]) != str:
-        columns_to_cast_to_integers.append('filename')
+        columns_to_cast_to_integers.append('file_id')
     df_out = df_out.astype({k: 'int' for k in columns_to_cast_to_integers})
     df_out.to_csv(f'{args.output}', index=False)
 
-#_______________________________________________________________
+#PCA___________________________________________________________________________________________
+    if args.pca == 'y' and len(df_out)>2:
+        feature_columns = [c for c in feature_df.columns if c[:2]!= 'n_' and c!='file_id']
+        X = feature_df[feature_columns]
+        pca = PCA(n_components=2)
+        components = pca.fit_transform(X)
+        fig = px.scatter(components, x=0, y=1)
+        fig.write_image("pca_plot.png")
+
+#______________________________________________________________________________________________
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default=None, type=str)
-    parser.add_argument('--output', default='output.csv', type=str)
-    parser.add_argument('--overwrite_output_dir', default=True, type=bool)
-    parser.add_argument('--statistics', default=True, type=bool)
-    parser.add_argument('--distributions', default=True, type=bool)
-    parser.add_argument('--lexical_richness', default=True, type=bool)
-    parser.add_argument('--readability', default=True, type=bool)
-    parser.add_argument('--pos_distribution', default=True, type=bool)
-    parser.add_argument('--text_column', default='text', type=str)
-
+    parser.add_argument('--input', default=None, type=str, help='director to input file or path (it is recommended to use this pipeline on texts > 100 tokens)')
+    parser.add_argument('--input_format', default=None, type=str, choices=['csv', 'xlsx', 'txt', 'folder_with_txt'], help='type of input data')
+    parser.add_argument('--output', default='output.csv', type=str, help='path to output, default=output.csv')
+    parser.add_argument('--overwrite_output_dir', default='t', type=str, choices=['y', 'n'], help='overwrite output file (y/n)')
+    parser.add_argument('--statistics', default='y', type=str, choices=['y', 'n'], help="compute length features (y/n)")
+    parser.add_argument('--distributions', default='y', type=str, choices=['y', 'n'], help="compute (PoS, punctuation, function word) distributions (y/n)")
+    parser.add_argument('--lexical_richness', default='y', type=str, choices=['y', 'n'], help='compute lexical richness scores (y/n)')
+    parser.add_argument('--readability', default='y', type=str, choices=['y', 'n'], help='compute readability scores (y/n)')
+    parser.add_argument('--pca', default='y', type=str, choices=['y', 'n'], help='perform PCA (y/n)')
+    parser.add_argument('--text_column', default='text', type=str, help='column that contains text input (only relevant if --input_format == "csv" or "xlsx")') 
     args = parser.parse_args()
     main(args)
--- util.py
+++ util.py
@@ -16,74 +16,74 @@ def get_n_syllables(word):
 
 #LEXICAL_RICHNESS_SCORES_____________________________________________________________
 def ttr(n_types,n_tokens):
-	return n_types/n_tokens
+	return round(n_types/n_tokens, 5)
 
 def rttr(n_types,n_tokens):
-	return n_types/sqrt(n_tokens)
+	return round(n_types/sqrt(n_tokens), 5)
 
 def cttr(n_types,n_tokens):
-	return n_types/sqrt(2*n_tokens) 
+	return round(n_types/sqrt(2*n_tokens), 5) 
 
 def Herdan(n_types,n_tokens):
-	return log(n_types)/log(n_tokens)
+	return round(log(n_types)/log(n_tokens), 5)
 
 def Summer(n_types,n_tokens):
-	return log(log(n_types))/log(log(n_tokens))
+	return round(log(log(n_types))/log(log(n_tokens)), 5)
 
 def Dugast(n_types,n_tokens):
-	return (log(n_tokens)**2)/(log(n_tokens)-log(n_types))
+	return round((log(n_tokens)**2)/(log(n_tokens)-log(n_types)), 5)
 
 def Maas(n_types,n_tokens):
-	return (log(n_tokens)-log(n_types))/(log(n_tokens)**2) 
+	return round((log(n_tokens)-log(n_types))/(log(n_tokens)**2), 5) 
 
 #READABILITY SCORES__________________________________________________________________
 def ARI(n_char, n_tokens, n_sentences):
-	return 4.71*(n_char/n_tokens) + 0.5*(n_tokens/n_sentences) - 21.43
+	return round(4.71*(n_char/n_tokens)+0.5*(n_tokens/n_sentences)-21.43, 5)
 
 def ColemanLiau(tokens, tokenized_sentences):
 	if len(tokens) < 100:
-		return None
+		return 0
 	chunks = [tokens[i:i+100] for i in range(0, len(tokens), 100) if i+100<=len(tokens)]
 	L = mean([len(''.join(chunk)) for chunk in chunks]) # avg. n char per 100 tokens
 	S = len(tokenized_sentences)/len(tokens)*100 # avg. n sent per 100 tokens
-	return 0.0588*L - 0.296*S - 15.8
+	return round(0.0588*L-0.296*S-15.8, 5)
 
 def Flesch(ASL, ASW):
-	return 206.835 - (1.015*ASL) - (84.6*ASW)
+	return round(206.835-(1.015*ASL)-(84.6*ASW), 5)
 
 def Fog(ASL, syllables):
 	syllables = [s for sent in syllables for s in sent]
 	if len(syllables) < 100:
-		return None
+		return 0
 	PHW = len([s for s in syllables if s >= 3])/len(syllables) # percentage hard words
-	return 0.4*(ASL + PHW)
+	return round(0.4*(ASL + PHW), 5)
 
 def Kincaid(ASL, ASW):
-	return (0.39*ASL) + (11.8*ASW) - 15.59
+	return round((0.39*ASL)+(11.8*ASW)-15.59, 5)
 
 def LIX(n_tokens, n_sentences, n_long_tokens):
-	return (n_tokens/n_sentences) + (n_long_tokens*100/n_tokens)
+	return round((n_tokens/n_sentences)+(n_long_tokens*100/n_tokens), 5)
 
 def RIX(n_long_tokens, n_sentences):
-	return n_long_tokens/n_sentences
+	return round(n_long_tokens/n_sentences, 5)
 
 def SMOG(sample):
 	length = len(sample)
 	if length < 30:
-		return None
+		return 0
 	else:
 		i = floor(length/3)
 		sample = sample[:10] + sample[i:i+10] + sample[-10:]
 		sample = [s for sent in sample for s in sent]
 	n_polysyllabic = len([s for s in sample if s > 2])
-	return 3 + sqrt(n_polysyllabic)
+	return round(3+sqrt(n_polysyllabic), 5)
 
 #DISTRIBUTIONS_______________________________________________________________________
 def get_word_length_distribution(tokens):
 	"""returns {token_length: rel_freq}"""
 	lengths = [len(t) for t in tokens]
 	dist = dict(Counter(lengths))
-	dist = {k:v/len(tokens) for k,v in dist.items()}
+	dist = {k:round(v/len(tokens), 5) for k,v in dist.items()}
 	dist = dict(sorted(dist.items(), key=operator.itemgetter(1),reverse=True))
 	return dist
 
@@ -93,7 +93,7 @@ def get_grapheme_distribution(tokens):
 	n_total = len(graphemes)
 	dist = dict(Counter(graphemes))
 	for k,v in dist.items():
-		dist[k] = v/n_total
+		dist[k] = round(v/n_total, 5)
 	dist = dict(sorted(dist.items(), key=operator.itemgetter(1),reverse=True))
 	return dist
 
@@ -111,15 +111,17 @@ def get_word_internal_grapheme_profile(tokens):
 				else:
 					profile[g] = 1
 	
-	profile = {k:v/n_tokens for k,v in profile.items()}
+	profile = {k:round(v/n_tokens, 5) for k,v in profile.items()}
 	profile = dict(sorted(profile.items(), key=operator.itemgetter(1),reverse=True))
 	return profile
 
-def get_token_distribution(tokens):
-	"""returns {token: rel_freq}"""
-	n_tokens = len(tokens)
-	dist = dict(Counter(tokens))
-	dist = {k:v/n_tokens for k,v in dist.items()}
+def get_function_word_distribution(doc):
+	"""returns {function_word: rel_freq}"""
+	allowed_pos = {'ADP', 'AUX', 'CCONJ', 'DET', 'PART', 'PRON', 'SCONJ'}
+	function_words = [w.text.lower() for s in doc.sentences for w in s.words if w.upos in allowed_pos]
+	n_function_words = len(function_words)
+	dist = dict(Counter(function_words))
+	dist = {k:round(v/n_function_words, 5) for k,v in dist.items()}
 	dist = dict(sorted(dist.items(), key=operator.itemgetter(1),reverse=True))
 	return dist
 
@@ -138,9 +140,11 @@ def get_grapheme_positional_freq(tokens):
 					grapheme_freq[t[i-1]] += 1
 				else:
 					grapheme_freq[t[i-1]] = 1
-		grapheme_freq = {k:v/n_tokens for k,v in grapheme_freq.items()}
+
+		grapheme_freq = {k:round(v/n_tokens, 5) for k,v in grapheme_freq.items()}
 		grapheme_freq = dict(sorted(grapheme_freq.items(), key=operator.itemgetter(1),reverse=True))
 		profile[i] = grapheme_freq
+
 	return profile
 
 def get_punct_dist(text):
@@ -151,25 +155,14 @@ def get_punct_dist(text):
 
 	n_char = len(text.replace(' ', ''))
 
-	dist_by_char = {k:v/n_char for k,v in dist.items()}
+	dist_by_char = {k:round(v/n_char, 5) for k,v in dist.items()}
 	dist_by_char = dict(sorted(dist_by_char.items(), key=operator.itemgetter(1),reverse=True))
 	return dist_by_char
 
 def get_positional_word_profile(doc):
-	"""returns {token_idx_in_sent: {word: rel_freq}}"""
-	tokens = [[w.text.lower() for w in s.words] for s in doc.sentences]
-	n_positions = max([len(s) for s in tokens])
-	profile = {}
-	
-	for i in range(n_positions):
-		k = i
-		words = [s[k] for s in tokens if len(s)>k]
-		n_sentences = len(words)
-		v = dict(Counter(words))
-		v = {k:v/n_sentences for k,v in v.items()}
-		v = dict(sorted(v.items(), key=operator.itemgetter(1),reverse=True))
-		profile[k] = v
-
+	"""returns counter of PoS tags that occur at the start of each sentence"""
+	tokens = [w.upos for s in doc.sentences for i,w in enumerate(s.words) if i==0]
+	profile = dict(Counter(tokens))
 	return profile
 
 def get_bigram_profile(tokens):
@@ -178,3 +171,16 @@ def get_bigram_profile(tokens):
 	profile = {', '.join(bigram) : bigrams.count(bigram) for bigram in bigrams}
 	profile = dict(sorted(profile.items(), key=operator.itemgetter(1),reverse=True))
 	return profile
+
+def get_pos_distribution(parsed_sentences, ngram_range=1):
+	"""returns {pos_ngram: rel_freq}"""
+	assert ngram_range > 0
+	if ngram_range==1:
+		pos_tag_ngrams = [pos for s in parsed_sentences for t, pos in s]
+	else:
+		tags = [pos for s in parsed_sentences for t, pos in s]
+		pos_tag_ngrams = [tags[i]+','+tags[i+1] for i in range(0, len(tags)-1)]
+	n_total = len(pos_tag_ngrams)
+	pos_distribution = dict(Counter(pos_tag_ngrams))
+	pos_distribution = {k:round(v/n_total, 5) for k,v in pos_distribution.items()}
+	return pos_distribution
