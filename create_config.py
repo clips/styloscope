21540d796f5cb6be63806da183fedc5f4a50c109
LemmensJens <jenslemmens97@gmail.com>
2023-09-07 16:51:01 +0200

    update requirements and config

--- create_config.py
+++ create_config.py
@@ -3,24 +3,24 @@ from configparser import ConfigParser
 config_object = ConfigParser()
 
 config_object["FEATURE_CONFIG"] = {
-    "stats": 1,
-    "distributions": 1,
-    "pos_ngram_range": '(1,1)',
-    "token_ngram_range": '(1,1)',
-    "readability": 1,
-    "lexical_richness": 1,
-    "pca": 1
+    "stats": 1, # 1 or 0
+    "distributions": 1, # 1 or 0
+    "pos_ngram_range": '(1,1)', # 1 or 0
+    "token_ngram_range": '(1,1)', # 1 or 0
+    "readability": 1, # 1 or 0
+    "lexical_richness": 1, # 1 or 0
+    "pca": 1 # 1 or 0
 }
 
 config_object["INPUT_CONFIG"] = {
-    "input": './personae',
-    "input_format": 'folder_with_txt',
-    "text_column": 'text' #if input_format=csv
+    "input": './personae', #.csv file or path to zip folder
+    "input_format": 'folder_with_txt', # csv or zip
+    "text_column": 'text' #only relevant if input_format==csv
 }
 
 config_object["OUTPUT_CONFIG"] = {
     "output_dir": 'output.csv',
-    "overwrite_output_dir": True
+    "overwrite_output_dir": '1' # 1 or 0
 }
 
 with open('config.ini', 'w') as conf:
